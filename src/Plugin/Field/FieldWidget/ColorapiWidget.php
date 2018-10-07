85ee9b7a88f8922d25cc9752e5b40aed2ea99907
Jay Friendly <friendly@jaypan.com>
2018-10-07 11:27:17 +1100

    Creating ColorapiWidgetBase class, that widgets can use as a base.

--- src/Plugin/Field/FieldWidget/ColorapiWidget.php
+++ src/Plugin/Field/FieldWidget/ColorapiWidget.php
@@ -2,10 +2,7 @@
 
 namespace Drupal\colorapi\Plugin\Field\FieldWidget;
 
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Field\WidgetInterface;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * The default widget for Color fields.
@@ -18,40 +15,4 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class ColorapiWidget extends WidgetBase implements WidgetInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary() {
-
-    $summary = [];
-
-    $summary['overview'] = $this->t('A textfield input that accepts a hexadecimal color string');
-
-    return $summary;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-
-    $element['name'] = [
-      '#type' => 'textfield',
-      '#title' => t('Name'),
-      '#default_value' => isset($items[$delta]) ? $items[$delta]->getColorName() : '',
-      '#cardinality' => $this->fieldDefinition->getFieldStorageDefinition()->getCardinality(),
-    ];
-
-    $element['color'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Color'),
-      '#default_value' => isset($items[$delta]) ? $items[$delta]->getHexadecimal() : '',
-      '#cardinality' => $this->fieldDefinition->getFieldStorageDefinition()->getCardinality(),
-    ];
-
-    return $element;
-  }
-
-}
+class ColorapiWidget extends ColorapiWidgetBase implements WidgetInterface {}
